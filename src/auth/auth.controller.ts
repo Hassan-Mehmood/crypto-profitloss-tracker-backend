e90557920d0ca6df3d87b5e4f70e3a34d9c21372
Hassan-Mehmood <hassanafridi1913@gmail.com>
2025-02-23 20:39:17 +0500

    Basic auth with jwt done

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { SigninUserDto } from 'src/users/schemas/signinUserSchema';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('signin')
+  async signIn(@Body() body: SigninUserDto) {
+    const response = await this.authService.signin(body);
+
+    return response;
+  }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,50 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { SigninUserDto } from 'src/users/schemas/signinUserSchema';
+import { JwtService } from '@nestjs/jwt';
+import { PrismaService } from 'src/prisma.service';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private prisma: PrismaService,
+    private jwtService: JwtService,
+  ) {}
+
+  async signin({ username, password }: SigninUserDto) {
+    try {
+      const user = await this.prisma.user.findUnique({
+        where: {
+          username: username,
+        },
+      });
+
+      if (!user) {
+        throw new UnauthorizedException('Username does not exists');
+      }
+
+      const isPasswordValid = await bcrypt.compare(password, user.password);
+
+      if (!isPasswordValid) {
+        throw new UnauthorizedException('Username or password is incorrect');
+      }
+
+      const payload = { sub: user.id, username: user.username };
+
+      return {
+        access_token: this.jwtService.sign(payload),
+      };
+    } catch (error) {
+      if (error instanceof UnauthorizedException) {
+        throw error;
+      }
+
+      console.error('Error during Auth signin:', error);
+      throw new InternalServerErrorException('Failed to sign in');
+    }
+  }
+}
