7488dead5665e58775dded04c162be9198baf566
Hassan-Mehmood <hassanafridi1913@gmail.com>
2025-02-23 19:19:48 +0500

    Basic signin route done

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -3,6 +3,7 @@ import { PrismaService } from 'src/prisma.service';
 import { UsersService } from './users.service';
 import { ZodValidationPipe } from './pipes/zodValidationPipe';
 import { SignupUserDto, signupUserSchema } from './schemas/signupUserSchema';
+import { SigninUserDto, signinUserSchema } from './schemas/signinUserSchema';
 
 @Controller('users')
 export class UsersController {
@@ -14,11 +15,15 @@ export class UsersController {
   @Post('/signup')
   @UsePipes(new ZodValidationPipe(signupUserSchema))
   async signUp(@Body() body: SignupUserDto) {
-    const response = await this.userService.signUp(
-      body.email,
-      body.username,
-      body.password,
-    );
+    const response = await this.userService.signUp(body);
+
+    return response;
+  }
+
+  @Post('/signin')
+  @UsePipes(new ZodValidationPipe(signinUserSchema))
+  async signIn(@Body() body: SigninUserDto) {
+    const response = await this.userService.signin(body);
 
     return response;
   }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,17 +1,20 @@
 import {
   ConflictException,
+  UnauthorizedException,
   Injectable,
   InternalServerErrorException,
 } from '@nestjs/common';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 import * as bcrypt from 'bcryptjs';
 import { PrismaService } from 'src/prisma.service';
+import { SignupUserDto } from './schemas/signupUserSchema';
+import { SigninUserDto } from './schemas/signinUserSchema';
 
 @Injectable()
 export class UsersService {
   constructor(private prisma: PrismaService) {}
 
-  async signUp(username: string, email: string, password: string) {
+  async signUp({ username, email, password }: SignupUserDto) {
     const saltRounds = 12;
     const hashedPassword = await bcrypt.hash(password, saltRounds);
 
@@ -43,6 +46,35 @@ export class UsersService {
 
       console.error('Error during signup:', error);
       throw new InternalServerErrorException('Failed to create user account');
+    }
+  }
+
+  async signin({ username, password }: SigninUserDto) {
+    try {
+      const user = await this.prisma.user.findUnique({
+        where: {
+          username: username,
+        },
+      });
+
+      if (!user) {
+        throw new UnauthorizedException('Username does not exists');
+      }
+
+      const isPasswordValid = await bcrypt.compare(password, user.password);
+
+      if (!isPasswordValid) {
+        throw new UnauthorizedException('Username or password is incorrect');
+      }
+
+      return user;
+    } catch (error) {
+      if (error instanceof UnauthorizedException) {
+        throw error;
+      }
+
+      console.error('Error during signin:', error);
+      throw new InternalServerErrorException('Failed to sign in');
     }
   }
 }
